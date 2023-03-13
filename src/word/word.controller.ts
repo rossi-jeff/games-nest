55d57ee660977d161b0a3a3785dc14262e43f56b
Jeff Rossi <jrossi@intouchgps.com>
2023-03-13 14:14:06 -0400

    word controller methods

--- src/word/word.controller.ts
+++ src/word/word.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { RandomWordDto } from '../global/dto/random-word.dto';
+import { WordService } from './word.service';
 
 @Controller('word')
-export class WordController {}
+export class WordController {
+  constructor(private wordService: WordService) {}
+
+  @Get(':id')
+  async findById(@Param('id') id: string) {
+    return await this.wordService.findById(parseInt(id));
+  }
+
+  @Post('random')
+  async randomWord(@Body() dto: RandomWordDto) {
+    return await this.wordService.randomWord(dto);
+  }
+}
